1879bee23c84976c2587fe565e31275018057db0
dgamliel <dgamliel@umail.ucsb.edu>
2019-10-07 13:19:31 -0700

    basic networking

---
+++ networkscan.py
@@ -0,0 +1,27 @@
+import socket
+import struct
+import fcntl
+
+#Get the IP mask
+iface = "eth0"
+_mask = socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 35099, struct.pack('256s', iface))[20:24])
+
+#Get the local ip range 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+
+#Grab the prefix
+loc_ip_prefix = s.getsockname()[0].split('.')[:2]
+'.'.join(loc_ip_prefix)
+
+mask = [~int(_byte) & 0xff for _byte in _mask.split('.')]
+
+#Search IP's from X.X.0.0 to X.X.255.255
+for i in range(
+
+
+
+print(s.getsockname()[0])
+print(_mask)
+s.close()
+
