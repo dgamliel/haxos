5920c9a59b753c45141f9a9967303304db3e815a
dgamliel <dgamliel@umail.ucsb.edu>
2019-10-28 21:39:28 -0700

    checking non empty list

--- driver.py
+++ driver.py
@@ -14,6 +14,7 @@ def test(connection):
 def tryConnection(socket, openDevices):
 	try:
 		deviceIP = openDevices[0]
+		print("Attempting to connect to", deviceIp, PORT)
 		socket.connect((deviceIP, PORT))
 	except:
 		print("Trying to connect to ", deviceIP)
@@ -23,19 +24,17 @@ def __main__():
 	#Returns list of all other pis on the network
 	openDevices = network.scanForPis()
 
+	if len(openDevices) == 0:
+		print("NO DEVICES FOUND")
+
 	#Set up my listener socket
 	mainSock = socket.socket()	
 	localIP = socket.gethostbyname(socket.gethostname())
 	mainSock.bind((localIP, PORT))
 	mainSock.listen(10)
 
-
 	sockets = [socket.socket() for i in range(5)]
-	
-	#Dummy connection to see if it works
 	trySock = sockets[0]
-	getHost = openDevices[0], PORT
-	trySock.connect((getHost))	
 
 	while True:
 		threading.Thread(target=tryConnection, args=(trySock, openDevices)).start()
