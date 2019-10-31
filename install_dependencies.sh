412894331b499bee43d2d436c329811e61752e82
dgamliel <dgamliel@umail.ucsb.edu>
2019-10-31 10:28:32 -0700

    bluetooth file to sense distance

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,7 +2,18 @@
 echo "DEPENDENCY: getmac ... INSTALLING"
 pip3 install getmac
 echo "DEPENDENCY: getmac ... DONE"
+
 export PATH="$PATH:/home/pi/.local/bin"
 echo "PATH EXPORTED ... /home/pi/.local/bin"
+
+echo "DEPENDENCY: Libbluetooth-dev ... INSTALLING"
+sudo apt-get install libbluetooth-dev
+echo "DEPENDENCY: Libbluetooth-dev ... DONE"
+
+
+echo "DEPENDENCY: Pybluez ... INSTALLING"
+pip3 install pybluez
+echo "DEPENDENCY: Pybluez ... DONE"
+
 eval `./install_dependencies.sh`
 echo "EVAL COMPLETED ... ./install_dependencies.sh"
