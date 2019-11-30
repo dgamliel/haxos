c1c24361f5f23a13cbb1e18bb2eb0a502c46a104
dgamliel <dgamliel@umail.ucsb.edu>
2019-11-29 23:01:51 -0800

    modified process network thread

--- new_driver.py
+++ new_driver.py
@@ -31,11 +31,244 @@ sendMap = {}
 pVals = paxos.paxosValues()
 
 #Map the IP to the socket we need to send on
-socketMap = {}
+#socketMap = {}
+ipAddrs = set()
 
 #My IP
 localIP = network.get_ip()
 PORT    = 10000
+
+
+'''
+# ballot = <Num, pid, depth of block>
+# "An acceptor doesnot accept, prepare, or accept messages from a contending leader
+# if the depth of the block being proposed is lower than the acceptor’s depth of
+# its copy of the blockchain"
+'''
+#Paxos info
+pid          = boot.getPiNum()
+ballot       = [0,pid]
+acceptBallot = [0,0]
+acceptVal    = ""
+depth        = 0
+
+#Maintaining paxos data as messages come in
+acceptCountDict = {} #key=ballot, value = acceptCount
+
+#This is what we will propose in phase II from leader
+initialVal    = None
+proposingBool = False
+phaseTwoList  = []
+
+
+def getSocketFromMessage(msg):
+
+    global sendMap
+
+    _json = json.loads(msg)
+    dest  = _json[dest]
+
+    return sendMap[dest]
+
+
+
+def processNetworkData():
+
+    global lock
+    lock.acquire()
+
+    global ackCount
+    global ballot
+    global phaseTwoList
+    global acceptBallot
+    global acceptVal
+    global acceptCountDict
+    global sendQueue
+    global transactions
+    global proposingBool
+    global depth
+    global blockChain
+    global chainList
+    global chainsRecevied
+
+    #Load json msg and get state
+    _json = json.loads(msg)
+    state = _json["state"]
+
+    if state == "PREPARE" :
+        receivedBal = _json["ballot"]
+
+        #Cannot accept smaller ballots in the future
+        if receivedBal >= ballot:
+            ballot[0] = receivedBal[0]
+            dest = _json["src"]
+            _json = jsonMsg(name,dest,state="ACK",ballot = receivedBal,acceptBallot=acceptBallot,acceptVal=acceptVal)	
+            sendQueue.put(_json)
+
+
+
+        #If bal smaller than myBal --> Don't respond
+        else:
+            lock.release()
+            return
+
+    elif state == "ACCEPT":
+
+        receivedBal = _json["ballot"]
+        receivedV = _json["x_y_coord"]
+
+        #Check if we received the ballot before
+        #if first time receiving ballot --> set ballot count to 1
+        #else --> increment ballot count
+        if str(receivedBal) not in acceptCountDict.keys():
+            acceptCountDict[str(receivedBal)] = 1
+
+        else:
+            acceptCountDict[str(receivedBal)] += 1
+
+        acceptCount = acceptCountDict[str(receivedBal)]
+
+        if acceptCount == 1: #case (not leader)
+
+            #received ballot >= currentballot number, then we "commit" to that value
+            if receivedBal[0] >= ballot[0]:
+
+                acceptBallot = receivedBal
+                acceptVal = receivedV
+
+                src=  _json["src"]
+                _json = jsonMsg(name,src,ballot=receivedBal,x_y_coord=acceptVal,state="ACCEPT")
+                sendQueue.put(_json)
+                lock.release()
+                return
+
+        if acceptCount==3: #case (leader)
+            for dest in ipAddrs:
+                _json = jsonMsg(name,dest,ballot=receivedBal,x_y_coord=receivedV,state="DECIDE")
+                sendQueue.put(_json)
+            #TODO: Update self values so that we consider a message sent to ourselves
+
+            #remove block from transaction queue
+            #only pop if proposing own value!!!!!!!!!!
+            if(receivedV==initialVal):
+                ackCount = 0
+
+            #set proposing to false
+            proposingBool=False
+
+
+    #We have decided the value and will append the block to the blockchain
+    elif state == "DECIDE": 
+
+        # _variable indicates variable from the received JSON
+        x_y_coord = _json["x_y_coord"]
+
+        #TODO: WRITE TO FILE
+        with open('paxos.log', 'a+') as f:
+            f.write(x_y_coord)
+            f.close()
+
+        #reset paxos vals for next round
+        proposingBool = False
+        acceptBallot = [0,0]
+        acceptVal = ""
+        ackCount = 0
+
+        #call paxos to see if it should run
+        paxos()
+
+
+
+    elif state == "ACK":
+        ackCount += 1
+        if ackCount == 2:
+            threading.Thread(target=startTimer, args=()).start()
+
+        #Received from acceptor phase I --> leader phase II
+        receivedVal = _json["acceptVal"]
+        receivedBal = _json["acceptBallot"]
+
+        #acceptPair <-- acceptBallot , acceptval
+        acceptPair = [receivedBal,receivedVal]
+        phaseTwoList.append(acceptPair)
+
+        #Received from majority
+        if len(phaseTwoList)==3:
+
+            #Vars to hold highest ballot and checking flags
+            myValChosen = True
+            highestBal = [-1,-1]
+            myVal = None
+
+            for pair in phaseTwoList:
+
+                #case acceptor node has accepted value
+                pairVal = pair[1]
+                if(pairVal!=""):
+                    myValChosen = False #We were not accepted as the leader. Need to restart
+
+                    #find highestBallot
+                    if(pair[0]>highestBal):
+                        pairBal    = pair[0]
+                        highestBal = pairBal
+                        myVal      = pairVal
+
+                if myValChosen == True:
+                    myVal = initialVal
+
+
+#send accept, ballot,myVal to all
+                for dest in ipAddrs:
+                    _json = jsonMsg(name,dest,x_y_coord=myVal,ballot=ballot,state="ACCEPT")
+                    sendQueue.put(_json)
+
+
+    elif state == "ABORT":
+
+        #Reset paxos values
+        proposingBool = False
+        acceptBallot  = [0,0]
+        acceptVal     = ""
+        ackCount      = 0
+
+
+    lock.release()
+
+
+    '''
+    elif state == "PING":
+    #load up each block in the chain and send it
+    dest = _json["src"]
+
+    #Send the entire blockchain to whoever pinged
+    for block in blockChain.chain:
+    txA   = block.transactionList[0]
+    txB   = block.transactionList[1]
+    depth = block.header.blockDepth
+    _hash = block.header.hash
+    nonce = block.header.nonce
+
+    x_y_coord = jsonBlock(txA, txB, depth,_hash,nonce)
+    _json = jsonMsg(name, dest, x_y_coord, ballot=ballot, state="UPDATE")
+    sendQueue.put(_json)
+
+    elif state == "UPDATE":
+
+    theirBallot = _json["ballot"]
+    if theirBallot[0] > ballot[0]:
+    ballot[0] = theirBallot[0]
+
+    _block = stringToBlock(_json["x_y_coord"])
+    _depth = _block.header.blockDepth
+    if _depth-depth==1:
+    # print(_block)
+    depth= _depth
+    blockChain.add(_block)
+    '''
+
+
+
+
 
 def sendThread():
 
@@ -57,7 +290,6 @@ def recvThread(recvSock):
         recvMessage = recvSock.recv(1024).decode('utf-8')
 
         print("recvThread()::59 Received", recvMessage)
-
 
     return
 
@@ -83,13 +315,13 @@ def connectSend(openDevices, sendSockets):
     print("connectSend(): DONE ... Able to send messages to all pis")
 
 def waitRecvConnections():
-	global acceptedCount 
-	global TOTAL_PIS_CONNECTED 
+    global acceptedCount 
+    global TOTAL_PIS_CONNECTED 
 
-	while acceptedCount != OTHERPIS:
-		continue
+    while acceptedCount != OTHERPIS:
+            continue
 
-
+    print("waitRecvConnections(): DONE ... Able to receive messages from all pis")
 
 def startPaxos():
     global sendQueue
