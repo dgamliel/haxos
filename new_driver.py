4f6397a3131be796b1f45c89620528849f040bb3
dgamliel <dgamliel@umail.ucsb.edu>
2019-12-05 01:08:26 -0800

    fetch RSSI and use for paxos val

--- new_driver.py
+++ new_driver.py
@@ -390,7 +390,7 @@ def startPaxos():
     acceptVal = ""
     
 
-    initialVal = "<0.0," +str(MY_PI) +">"
+    initialVal = MYPI + " " + network.getRSSI()
 
     for dest in sendMap.keys():
         sendMessage = JSON.jsonMsg(me,dest,state="PREPARE",ballot=ballot)
